5ecf98f987cc3a205f15fb33890032b0b1404e0b
Cbutler1 <collin.p.butler1@gmail.com>
2022-08-29 13:43:15 -0400

    added notes

--- CountriesMVCLab/CountryController.cs
+++ CountriesMVCLab/CountryController.cs
@@ -11,30 +11,30 @@ namespace CountriesMVCLab
 
         public List<Country> CountryDb { get; set; } = new List<Country>()
         {
-            new Country("Albania", "continent", new List<string>(){"Red", "Black" }),
-            new Country("Angola", "continent", new List<string>(){"Red", "Black", "Gold" }),
-            new Country("Bahrain", "continent", new List<string>(){"Red", "White" }),
-            new Country("Belize", "continent", new List<string>(){"Red", "Blue", "White", "Green" }),
-            new Country("Bolivia", "continent", new List<string>(){"Red", "Yellow", "Green" }),
-            new Country("Cameroon", "continent", new List<string>(){"Red", "Green", "Yellow" }),
-            new Country("Ecuador", "continent", new List<string>(){"Red", "Blue", "Yellow" }),
-            new Country("Denmark", "continent", new List<string>(){"Red", "White" }),
-            new Country("Japan", "continent", new List<string>(){"Red", "White" }),
-            new Country("Jordan", "continent", new List<string>(){"Red", "Black", "Green", "White" }),
-            new Country("Kuwait", "continent", new List<string>(){"Red", "Black", "Green", "White" }),
-            new Country("Laos", "continent", new List<string>(){"Red", "Blue", "White" }),
-            new Country("Madagascar", "continent", new List<string>(){"Red", "White", "Green" }),
-            new Country("Mali", "continent", new List<string>(){"Red", "Yellow", "Green" }),
-            new Country("Monaco", "continent", new List<string>(){"Red", "White" }),
-            new Country("Poland", "continent", new List<string>(){"Red", "White" }),
-            new Country("Syria", "continent", new List<string>(){"Red", "Black", "White", "Green" }),
-            new Country("The United States", "continent", new List<string>(){"Red", "White", "Blue" })
+            new Country("Albania", "Europe", new List<string>(){"Red", "Black" }),
+            new Country("Angola", "Africa", new List<string>(){"Red", "Black", "Gold" }),
+            new Country("Bahrain", "Asia", new List<string>(){"Red", "White" }),
+            new Country("Belize", "North America", new List<string>(){"Red", "Blue", "White", "Green" }),
+            new Country("Bolivia", "South America", new List<string>(){"Red", "Yellow", "Green" }),
+            new Country("Cameroon", "Africa", new List<string>(){"Red", "Green", "Yellow" }),
+            new Country("Ecuador", "South America", new List<string>(){"Red", "Blue", "Yellow" }),
+            new Country("Denmark", "Europe", new List<string>(){"Red", "White" }),
+            new Country("Japan", "Asia", new List<string>(){"Red", "White" }),
+            new Country("Jordan", "Asia", new List<string>(){"Red", "Black", "Green", "White" }),
+            new Country("Kuwait", "Asia", new List<string>(){"Red", "Black", "Green", "White" }),
+            new Country("Laos", "Asia", new List<string>(){"Red", "Blue", "White" }),
+            new Country("Madagascar", "Africa", new List<string>(){"Red", "White", "Green" }),
+            new Country("Mali", "Africa", new List<string>(){"Red", "Yellow", "Green" }),
+            new Country("Monaco", "Europe", new List<string>(){"Red", "White" }),
+            new Country("Poland", "Europe", new List<string>(){"Red", "White" }),
+            new Country("Syria", "Asia", new List<string>(){"Red", "Black", "White", "Green" }),
+            new Country("The United States", "North America", new List<string>(){"Red", "White", "Blue" })
         };
 
         public void CountryAction(Country c)
         {
-            int selection = GetSelection();
-            var countryView = new CountryView(CountryDb[selection - 1]);
+            //dont understand where c comes from or how to use it
+            var countryView = new CountryView(c);
             countryView.Display();
         }
         public void WelcomeAction()
@@ -42,8 +42,14 @@ namespace CountriesMVCLab
             var listView = new CountryListView(CountryDb);
             Console.WriteLine("Hello, welcome to the country app.  Please select a country from the following list:");
             listView.Display();
-            GetSelection();
-            CountryActon();
+            Console.WriteLine("Please select a country by using the associated number.");
+            int selection = int.Parse(Console.ReadLine());
+            var countrySelection = new CountryView(CountryDb[selection - 1]);
+            CountryAction();
+
+            //really struggling to figure out how to make WelcomeAction and CountryAction talk together
+
+            
             
             
             
@@ -51,10 +57,7 @@ namespace CountriesMVCLab
             Console.WriteLine("Would you like to learn about another country? y/n");
         }
 
-        public int GetSelection()
-        {
-            Console.WriteLine("Please select a country by using the associated number.");
-            return int.Parse(Console.ReadLine());
-        }
+       
+        
     }
 }
